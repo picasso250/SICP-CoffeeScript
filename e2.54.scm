dfc08eb6421df9a1669e35446c8409f72ddc2ef0
Ryan <cumt.xiaochi@gmail.com>
2014-08-09 09:07:35 +0800

    1

--- e2.54.scm
+++ e2.54.scm
@@ -1,15 +1,3 @@
-Copyright (C) 2014 Massachusetts Institute of Technology
-This is free software; see the source for copying conditions. There is NO warranty; not even
-for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-Image saved on Monday May 19, 2014 at 9:55:33 PM
-  Release 9.2 || Microcode 15.1 || Runtime 15.7 || Win32 1.8 || SF 4.41 || LIAR/i386 4.118
-  Edwin 3.116
-;You are in an interaction window of the Edwin editor.
-;Type `C-h' for help, or `C-h t' for a tutorial.
-;`C-h m' will describe some commands.
-;`C-h' means: hold down the Ctrl key and type `h'.
-;Package: (user)
 
 (define (equal? a b)
   (cond ((not (pair? a))
